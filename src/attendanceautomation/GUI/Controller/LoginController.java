d539a34378ba8184996066320e2b2c87cf026db9
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2020-02-20 06:56:54 +0100

    gfdg

--- src/attendanceautomation/GUI/Controller/LoginController.java
+++ src/attendanceautomation/GUI/Controller/LoginController.java
@@ -7,21 +7,26 @@ package attendanceautomation.GUI.Controller;
 
 import attendanceautomation.GUI.Model.AttendanceAutomationModel;
 import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
  * @author BBran
  */
 public class LoginController implements Initializable {
-    
+
     private Label label;
     @FXML
     private TextField txtfieldUsername;
@@ -29,31 +34,24 @@ public class LoginController implements Initializable {
     private PasswordField passwordfieldPassword;
     @FXML
     private JFXButton btnLogIn;
-    
-   
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
-    }    
+    }
 
     @FXML
-    private void handleLogIn(ActionEvent event)
-    {
+    private void handleLogIn(ActionEvent event) {
         String username = txtfieldUsername.getText();
-                       
-        
-        if (username == "Teacher")
-        {
-            
-        }
-        
-        if (username == "Student")
-        {
-            
+
+        if (username.equals("Teacher")) {
+
         }
 
-        
-        
+        if (username.equals("Student")) {
+
+        }
+
     }
-    
+
 }
